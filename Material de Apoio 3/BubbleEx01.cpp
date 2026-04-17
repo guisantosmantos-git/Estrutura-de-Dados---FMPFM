70579dbac6f58f0322e337a82345e84f52fd32f0
guisantosmantos-git <guisantos.mantos@gmail.com>
2026-04-17 18:49:00 -0300

    Add files via upload

---
+++ Material de Apoio 3/BubbleEx01.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+void imprimeVetor(int vetor[]){
+    int i;
+    cout << endl;
+    for(i = 0;i < 5; i++){
+        cout << " | " << vetor[i] << " | ";
+    }
+}
+
+void bubbleSort3(int x[])
+{
+    int n = 1;
+    int troca = 1;
+    int aux = 0;
+    while (n <= 5 && troca == 1)
+    {
+        troca = 0;
+        for (int i = 0; i<=3; i++)
+        {
+            if (x[i] > x[i + 1])
+            {
+                troca = 1;
+                aux = x[i];
+                x[i] = x[i + 1];
+                x[i + 1] = aux;
+            }
+        }
+        n++;
+    }
+}
+
+int main()
+{
+int x[5];
+
+for (int i = 0; i <= 4; i++)
+{
+    cout << "Digite o numero: " << endl;
+    cin >> x[i];
+}
+
+imprimeVetor(x);
+cout << endl;
+bubbleSort3(x);
+imprimeVetor(x);
+
+return 0;
+}
